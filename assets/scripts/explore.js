6fbf53e3b1c5bfcb12fd85398d6e774dfb2d6709
noahdj37 <noahdj37@gmail.com>
2023-05-04 16:13:15 -0700

    Finish explore.js
    finished speech synthesis task

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,44 @@
 // explore.js
-
+const synth = window.speechSynthesis;
+let select, button, voices, image;
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  select = document.getElementById('voice-select');
+  button = document.getElementsByTagName('button')[0];
+  image = document.querySelector("[alt='Smiling face']");
+  console.log(button.innerHTML);
+
+  //Get voices and append to dropdown
+  setTimeout(() => {
+    voices = synth.getVoices();
+
+    for(let i = 0; i < voices.length; i++){
+      let option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      option.setAttribute('value', voices[i].name);
+      select.appendChild(option);
+    }
+  }, 100);
+
+  button.addEventListener('click', function(){
+    speakText();
+  });
+}
+
+function speakText(){
+  let utterance = new SpeechSynthesisUtterance(document.getElementById('text-to-speak').value);
+  for(let i = 0; i < voices.length; i++){
+    if(voices[i].name == select.value){
+      utterance.voice = voices[i];
+      break;
+    }
+  }
+  image.setAttribute('src', 'assets/images/smiling-open.png');
+  setInterval(() => {
+    if(!synth.speaking){
+      image.setAttribute('src', 'assets/images/smiling.png');
+    }
+  }, 100);
+  synth.speak(utterance);
 }
